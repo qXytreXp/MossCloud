b43abf62146c805d7b67dcf3fdf5a017be3327e4
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-10-28 15:10:00 +0200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MainView.as_view(), name='main'),
+    path('', views.MainPageView.as_view(), name='main'),
     path('download/<str:filename>/', views.DownloadDocumentView.as_view(), name='download'),
     path('delete/<str:filename>/', views.DeleteDocumentView.as_view(), name='delete'),
     path('add-file/', views.AddDocumentView.as_view(), name='add_file'),
