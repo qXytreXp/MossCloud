d0a3a11313b43d292590d12a2c6c58894e477432
IllyaMoss <50335232+qXytreXp@users.noreply.github.com>
2020-11-22 21:03:06 +0200

    Update storageApi.py

--- main/storageApi.py
+++ main/storageApi.py
@@ -1,30 +1,30 @@
 import requests
+from django.conf import settings
        
 
-session = requests.Session()
-address = 'http://127.0.0.1:8000'
-token = '43880072'    
+session = requests.Session()  
 
 
 def add_document(username, document):
-    add_doc = session.post(f'{address}/api/main/add-doc/', data={'username':username, 'token':token}, files={'document':document})
+    add_doc = session.post(f'{settings.STORAGE_API_URL}/api/main/add-doc/', data={'username':username, 'token':settings.STORAGE_API_TOKEN}, files={'document':document})
 
     return add_doc
 
 
 def download_doc(username, document):
-    download_doc_url = session.get(f'{address}/api/main/download-doc/{username}/{token}/{document}')
+    download_doc_url = session.get(f'{settings.STORAGE_API_URL}/api/main/download-doc/{username}/{settings.STORAGE_API_TOKEN}/{document}')
 
     return download_doc_url
 
 
 def get_my_document(username):
-    get_doc = session.post(f'{address}/api/main/storage/', json={'username':username, 'token':token})
+    get_doc = session.post(f'{settings.STORAGE_API_URL}/api/main/storage/', json={'username':username, 'token':settings.STORAGE_API_TOKEN})
 
     return get_doc.json()
     
     
 def delete_document(username, document):
-    del_doc = session.post(f'{address}/api/main/delete-doc/', json={'username':username, 'token':token, 'document':document})
+    del_doc = session.post(f'{settings.STORAGE_API_URL}/api/main/delete-doc/', json={'username':username, 'token':settings.STORAGE_API_TOKEN, 'document':document})
 
     return del_doc.json()
+
